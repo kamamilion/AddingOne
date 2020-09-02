17bda8f4c23f609b4f1f681b591758ad6b9fd32a
kamamilion <70108303+kamamilion@users.noreply.github.com>
2020-09-02 09:26:59 +0200

    Add files via upload

--- AddingNumber.java
+++ AddingNumber.java
@@ -1,28 +1,60 @@
 package myProject;
 
 import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 
-public class AddingNumber {
-	 int number;
-	 
-	void setNumber(int n) {
-			number = n;
-		}
-	void IncreaseEachDigit() {
+public class AddingNumber extends JPanel implements ActionListener {
+	JButton button;
+	JTextField text;
+	JTextField result;
+	JPanel panel;
+	JLabel label;
+	int number;
+
+	
+	public void prepareGUI() {
+		JFrame frame = new JFrame();
+		panel = new JPanel();
+		label = new JLabel("Enter the number");
+		text = new JTextField(10);
+		button = new JButton ("Convert");
+		button.addActionListener(this);
+		result = new JTextField(20);
+		panel.add(label);
+		panel.add(text);
+		panel.add(button);
+		panel.add(result);
+		frame.getContentPane().add(BorderLayout.NORTH, panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800,200);
+		frame.setVisible(true);	
+	}
+	public void actionPerformed(ActionEvent event) {
+		String a = text.getText();
+		number = Integer.parseInt(a);
 		ArrayList<Integer> digits = new ArrayList<Integer>();
+		String s = null;
+		int x = 0;
 		while (number != 0) {
 			digits.add(number % 10);
 			number = number / 10; 
 			}
 		int i = digits.size() - 1;
 		while (i >= 0) {
-			System.out.print(digits.get(i) + 1);
+			x = x*10 + digits.get(i)+1;
 			i--;
-		}	
-}
+		}
+		s = String.valueOf(x);
+		result.setText(s);
+	}
+		
+
 	public static void main(String[] args) {
-		AddingNumber test = new AddingNumber();
-		test.setNumber(84398);
-		test.IncreaseEachDigit();
+		AddingNumber newGUI = new AddingNumber();
+		newGUI.prepareGUI();
+		
 	}
 }
